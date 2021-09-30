7151cd1bc9076e18ae879f16a89cee0e29f61d7f
Thiago Teixeira <thiagot@id.uff.br>
2021-09-30 15:06:45 -0300

    Cria Ciclo de vida

---
+++ Controllers/V1/CicloDeVidaIDController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace ApiCatalogoJogos.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CicloDeVidaIDController : Controller
+    {
+
+
+    }
+}
