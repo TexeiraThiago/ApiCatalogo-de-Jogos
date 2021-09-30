89a34a5eb1e6181e234456d70c3e7fde457fa06b
Thiago Teixeira <thiagot@id.uff.br>
2021-09-30 10:41:33 -0300

    Cria view model

--- Controllers/V1/JogosController.cs
+++ Controllers/V1/JogosController.cs
@@ -12,6 +12,25 @@ namespace ApiCatalogoJogos.Controllers.V1
     [ApiController]
     public class JogosController : ControllerBase
     {
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> Obter() => Ok();
+
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<object>> Obter(Guid idJogo) => Ok();
+
+        [HttpPost]
+        public async Task<ActionResult<object>> InserirJogo(object jogo) => Ok();
+
+        [HttpPut("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, object jogo) => Ok();
+
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, double preco) => Ok();
+
+        [HttpDelete("{idJogo:guid}")]
+        public async Task<ActionResult> ApagarJogo(Guid idJogo) => Ok();
 
     }
+
+
 }
